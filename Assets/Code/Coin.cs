f1f2886d62f7063567555fded2ece3863eb03bd9
kravening <sep_ul_tura@live.nl>
2015-09-14 10:09:19 +0200

    k

---
+++ Assets/Code/Coin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : Pickup//inherit van pickup
+{
+    public override void PlayerHit(Player _GO)
+    {
+        base.PlayerHit(_GO);//roep de basis classe nog wel aan(dus de playerhit in pickup)
+        //voeg hier code toe zo dat de speler zijn score omhoog gaat
+		_GO.coinCollected ();
+		Destroy (this.gameObject);
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private int m_score = 0;
+	private float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+		switch (m_score) {
+		case 2:
+			speed = 2;
+			break;
+		case 3:
+			transform.Rotate(0,0,1);
+			break;
+		}
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+	public void coinCollected(){
+		m_score += 1;
+		Debug.Log (m_score);
+	}
+
+}
